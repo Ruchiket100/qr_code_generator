4e7c2265a15763e89fe68e624d9d8d62f38b4d1a
Ruchiket Borse <RuchiketBorse1@gmail.com>
2024-08-08 13:58:13 +0000

    refactor

--- frontend/src/pages/QRGenerate.tsx
+++ frontend/src/pages/QRGenerate.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 export const QRGenerate = () => {
     const [url, setUrl] = React.useState('');
@@ -28,14 +30,21 @@ export const QRGenerate = () => {
     }
 
     return (
-        <div className='text-slate-900'>
-            <label>URL</label>
-            <input className='border border-slate-500' onChange={(r) => setUrl(r.target.value)} type="text" />
-            <br />
-            <label>IMG URL</label>
-            <input className='border border-slate-500' type="text" onChange={(r) => setImgUrl(r.target.value)} />
-            <button onClick={handleGenerate}>Generate</button>
+        <div className='flex flex-col gap-2 w-full h-full items-center justify-center '>
+            <h1 className='text-4xl px-4 py-8 font-extrabold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text'>QR Code Generator</h1>
+            <br/>
+            <div className='flex border rounded-lg p-4 flex-col w-full max-w-[500px] gap-1'>
+            <label className='text-sm italic font-extrabold'>URL</label>
+            <Input className='border border-slate-500' onChange={(r) => setUrl(r.target.value)} type="text" />
+            
+            <label className='text-sm italic font-extrabold'>IMG URL</label>
+            <Input className='border border-slate-500' type="text" onChange={(r) => setImgUrl(r.target.value)} />
+            <Button onClick={handleGenerate}>Generate</Button>
+            </div>
+            <div className='w-full  aspect-square md:w-[500px] flex items-center justify-center border-2 border-dashed rounded-lg'>
+            {!src && <h1 className=" text-center justify-center font-bold text-gray-500 text-lg">Result</h1>}
             {src && <img className='w-20 h-20' src={src} />}
+            </div>
         </div>
     );
 };
